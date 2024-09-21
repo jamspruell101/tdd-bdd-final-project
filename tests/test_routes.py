32faafdc33e47b6d604ca1bb94ac318dfff04b66
JaMon Spruell <jamspruell101@gmail.com>
2024-09-21 00:30:45 -0500

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -193,4 +193,22 @@ class TestProductRoutes(TestCase):
         new_count = self.get_product_count()
         self.assertEqual(new_count, product_count - 1)
 
+  ######################################################################
+# DELETE A PRODUCT
+######################################################################
+@app.route("/products/<int:product_id>", methods=["DELETE"])
+def delete_products(product_id):
+    """
+    Delete a Product
+
+    This endpoint will delete a Product based the id specified in the path
+    """
+    app.logger.info("Request to Delete a product with id [%s]", product_id)
+
+    product = Product.find(product_id)
+    if product:
+        product.delete()
+
+    return "", status.HTTP_204_NO_CONTENT
+
 
