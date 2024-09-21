2549f55b1cea1f5423b5bb60492deedaef4b0419
JaMon Spruell <jamspruell101@gmail.com>
2024-09-21 00:32:57 -0500

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -211,4 +211,26 @@ def delete_products(product_id):
 
     return "", status.HTTP_204_NO_CONTENT
 
+######################################################################
+# UPDATE AN EXISTING PRODUCT
+######################################################################
+@app.route("/products/<int:product_id>", methods=["PUT"])
+def update_products(product_id):
+    """
+    Update a Product
+
+    This endpoint will update a Product based the body that is posted
+    """
+    app.logger.info("Request to Update a product with id [%s]", product_id)
+    check_content_type("application/json")
+
+    product = Product.find(product_id)
+    if not product:
+        abort(status.HTTP_404_NOT_FOUND, f"Product with id '{product_id}' was not found.")
+
+    product.deserialize(request.get_json())
+    product.id = product_id
+    product.update()
+    return product.serialize(), status.HTTP_200_OK
+
 
